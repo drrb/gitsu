0a387c73b81599e6c17bb7c8fb3690ede82322de
drrb <drrb@github.com>
2013-04-07 17:12:29 +1000

    Don't record coverage when using Ruby 1.8.x

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,4 @@
-require 'simplecov'
+require 'simplecov' unless RUBY_VERSION.match /^1.8/
 
 lib_path = File.expand_path('../../../lib', __FILE__)
 $LOAD_PATH << lib_path unless $LOAD_PATH.include? lib_path
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
-require 'simplecov'
+require 'simplecov' unless RUBY_VERSION.match /^1.8/
 
 require 'gitsu'
