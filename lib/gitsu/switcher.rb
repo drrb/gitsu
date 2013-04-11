aa9aee868e540ec3ca53917568377b51330e0e36
drrb <drrrrrrrrrrrb@gmail.com>
2013-04-11 18:35:40 +1000

    Refactored switcher

--- lib/gitsu/switcher.rb
+++ lib/gitsu/switcher.rb
@@ -8,9 +8,10 @@ module GitSu
 
         def request(scope, *user_strings)
             begin
-                found_users = parse_or_find_all user_strings
-                found_user = combine_all found_users
-                select_user found_user, scope
+                parsed, not_parsed = try_to_parse_all user_strings
+                found = find_all not_parsed
+                user = combine_all(parsed + found)
+                select_user user, scope
             rescue RuntimeError => error
                 @output.puts error.message
             end
@@ -71,29 +72,38 @@ module GitSu
         end
 
         private
+        def try_to_parse_all(user_strings)
+            parsed = []
+            not_parsed = []
+            user_strings.each do |user_string|
+                begin
+                    parsed << User.parse(user_string)
+                rescue User::ParseError => parse_error
+                    not_parsed << user_string
+                end
+            end
+            return [parsed, not_parsed]
+        end
+
+        def find_all(user_strings)
+            user_strings.map do |user_string|
+                find user_string
+            end
+        end
+
+        def find(user_string)
+            found_user = @user_list.find(user_string)
+            if found_user.none?
+                raise "No user found matching '#{user_string}'"
+            else
+                found_user
+            end
+        end
+
         def combine_all(users)
             group_email = @config_repository.group_email_address
             users.inject(User::NONE) do |combined_user, user|
                 combined_user.combine user, group_email
-            end
-        end
-
-        def parse_or_find_all(user_strings)
-            user_strings.map do |user_string|
-                found_user = parse_or_find user_string
-                if found_user.none?
-                    raise "No user found matching '#{user_string}'"
-                else
-                    found_user
-                end
-            end
-        end
-
-        def parse_or_find(user_string)
-            begin
-                User.parse(user_string)
-            rescue User::ParseError => parse_error
-                @user_list.find(user_string)
             end
         end
 
